42d34eb568bcb735f9c3a32c68d35a1580995350
Kevin Tanuhardi <kevin.tanuhardi@gmail.com>
2021-08-03 21:50:03 +0700

    IMPROVE: private path checker

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -2,7 +2,29 @@ const jwtService = require('../services/jwtService');
 
 const { privateRoute } = require('../helpers/constant');
 
-const checkPrivateRoute = (method, path) => privateRoute[method].indexOf(path) !== -1;
+const checkPrivateRoute = (method, path) => {
+  const privWithParamsIndexArr = privateRoute[method].map((el, index) => (el.indexOf(':') !== -1 ? index : '')).filter(String);
+  const paramsRouteChecker = privateRoute[method].map((privPath, index) => {
+    if (privWithParamsIndexArr.indexOf(index) === -1) return false;
+    const privPathArr = privPath.split('/');
+    const currPathArr = path.split('/');
+    // const reqParamsIndex = privPathArr.findIndex((el) => el.indexOf(':') !== -1);
+    const reqParamsIndex = privPathArr.map((el, idx) => (el.indexOf(':') !== -1 ? idx : '')).filter(String);
+    let check = true;
+
+    if (privPathArr.length !== currPathArr.length) return false;
+
+    for (let i = 0; i < privPathArr.length; i++) {
+      // eslint-disable-next-line no-continue
+      if (reqParamsIndex.indexOf(i) !== -1) continue;
+      check = privPathArr[i] === currPathArr[i];
+      if (check === false) break;
+    }
+    return check === true;
+  });
+  if (paramsRouteChecker.indexOf(true) !== -1) return true;
+  return privateRoute[method].indexOf(path) !== -1;
+};
 
 module.exports = {
   validateToken: async (req, _, next) => {
